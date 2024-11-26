87a182d41c3b2d0f27645b8e79eb081a82715fac
Koketso-dax <lanchaster.k@gmail.com>
2024-11-26 17:03:06 +0200

    [Update] 7-httpjs

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -2,28 +2,64 @@
  * small express server to read from db and return plain text
  */
 const express = require('express');
-const countStudents = require('./3-read_file_async');
+const { readFile } = require('fs');
 
-const app = express()
-const PORT = 1245;
-const PATH = process.argv.length > 2 ? process.argv[2] : '';
+const app = express();
+const port = 1245;
 
-app.get('/', (_, res) => {
+function countStudents(fileName) {
+  const students = {};
+  const fields = {};
+  let length = 0;
+  return new Promise((resolve, reject) => {
+    readFile(fileName, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        let output = '';
+        const lines = data.toString().split('\n');
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            length += 1;
+            const field = lines[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
+            }
+          }
+        }
+        const l = length - 1;
+        output += `Number of students: ${l}\n`;
+        for (const [key, value] of Object.entries(fields)) {
+          if (key !== 'field') {
+            output += `Number of students in ${key}: ${value}. `;
+            output += `List: ${students[key].join(', ')}\n`;
+          }
+        }
+        resolve(output);
+      }
+    });
+  });
+}
+
+app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
-
-app.get('/students', (_, res) => {
-  countStudents(PATH)
-    .then((data) => {
-      res.send(`This is the list of our students\n${data.join('\n')}`);
-    })
-    .catch((error) => {
-      res.send(`This is the list of our students\n${error.message}`);
-    });
+app.get('/students', (req, res) => {
+  countStudents(process.argv[2].toString()).then((output) => {
+    res.send(['This is the list of our students', output].join('\n'));
+  }).catch(() => {
+    res.send('This is the list of our students\nCannot load the database');
+  });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+app.listen(port, () => {
 });
 
 module.exports = app;
