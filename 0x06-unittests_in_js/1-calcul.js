5478be1037f551b2524fcc6ec282aac0eaeb92ac
Koketso-dax <lanchaster.k@gmail.com>
2024-11-28 14:37:32 +0200

    [Add] 1-calcul.js

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,26 @@
+/**
+ * calculateNumber(a, b) - returns the sum | diff | mul | div of a and b.
+ * @param {String} type: operation to perform (SUM, SUBTRACT, DIVIDE)
+ * @param {Number} a: first number
+ * @param {Number} b: second number
+ * @returns {Number} result of the operation
+ */
+function calculateNumber(type, a, b) {
+  const aNum = Math.round(a);
+  const bNum = Math.round(b);
+  if (type === 'SUM') {
+    return aNum + bNum;
+  }
+  if (type === 'SUBTRACT') {
+    return aNum - bNum;
+  }
+  if (type === 'DIVIDE') {
+    if (bNum === 0) {
+      return 'Error';
+    }
+    return aNum / bNum;
+  }
+  return 'Error';
+}
+
+module.exports = calculateNumber;
---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,23 @@
+/**
+ * Tests calculateNumber in './1-calcul.js'
+ */
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', function () {
+  it('should return the sum of two rounded numbers', function () {
+    assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+  });
+
+  it('should return the difference of two rounded numbers', function () {
+    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+  });
+
+  it('should return the division of two rounded numbers', function () {
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+  });
+
+  it('should return "Error" when dividing by zero', function () {
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+  });
+});
